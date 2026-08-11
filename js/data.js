6694b34ace141c5193716c6887cf01e783381421
Ioannidis Ioannis <109786525+iioannidis94@users.noreply.github.com>
2026-08-11 14:14:09 +0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -675,4 +675,143 @@ const CITIES = [
   {name:"Sana'a",country:"Yemen",lat:15.3694,lng:44.1910},
   {name:"Aden",country:"Yemen",lat:12.7981,lng:45.0354},
   {name:"Taiz",country:"Yemen",lat:13.5795,lng:44.0205}
+
+// Australia (Major Cities & Regional Centres)
+  {name:"Sydney",country:"Australia",lat:-33.8688,lng:151.2093},
+  {name:"Melbourne",country:"Australia",lat:-37.8136,lng:144.9631},
+  {name:"Brisbane",country:"Australia",lat:-27.4698,lng:153.0251},
+  {name:"Perth",country:"Australia",lat:-31.9505,lng:115.8605},
+  {name:"Adelaide",country:"Australia",lat:-34.9285,lng:138.6007},
+  {name:"Gold Coast",country:"Australia",lat:-28.0167,lng:153.4000},
+  {name:"Newcastle",country:"Australia",lat:-32.9283,lng:151.7817},
+  {name:"Canberra",country:"Australia",lat:-35.2809,lng:149.1300},
+  {name:"Sunshine Coast",country:"Australia",lat:-26.6500,lng:153.0667},
+  {name:"Wollongong",country:"Australia",lat:-34.4244,lng:150.8931},
+  {name:"Hobart",country:"Australia",lat:-42.8821,lng:147.3272},
+  {name:"Geelong",country:"Australia",lat:-38.1499,lng:144.3617},
+  {name:"Townsville",country:"Australia",lat:-19.2590,lng:146.8169},
+  {name:"Cairns",country:"Australia",lat:-16.9186,lng:145.7781},
+  {name:"Darwin",country:"Australia",lat:-12.4634,lng:130.8456},
+  {name:"Toowoomba",country:"Australia",lat:-27.5598,lng:151.9507},
+  {name:"Ballarat",country:"Australia",lat:-37.5622,lng:143.8503},
+  {name:"Bendigo",country:"Australia",lat:-36.7570,lng:144.2794},
+  {name:"Albury",country:"Australia",lat:-36.0737,lng:146.9135},
+  {name:"Launceston",country:"Australia",lat:-41.4332,lng:147.1441},
+  {name:"Mackay",country:"Australia",lat:-21.1411,lng:149.1861},
+  {name:"Rockhampton",country:"Australia",lat:-23.3739,lng:150.5117},
+  {name:"Bunbury",country:"Australia",lat:-33.3256,lng:115.6396},
+  {name:"Coffs Harbour",country:"Australia",lat:-30.2963,lng:153.1141},
+  {name:"Bundaberg",country:"Australia",lat:-24.8662,lng:152.3489},
+  {name:"Wagga Wagga",country:"Australia",lat:-35.1119,lng:147.3698},
+  {name:"Hervey Bay",country:"Australia",lat:-25.2884,lng:152.8711},
+  {name:"Mildura",country:"Australia",lat:-34.2080,lng:142.1365},
+  {name:"Shepparton",country:"Australia",lat:-36.3833,lng:145.4000},
+  {name:"Gladstone",country:"Australia",lat:-23.8427,lng:151.2555},
+  {name:"Port Macquarie",country:"Australia",lat:-31.4308,lng:152.9069},
+  {name:"Tamworth",country:"Australia",lat:-31.0927,lng:150.9320},
+  {name:"Traralgon",country:"Australia",lat:-38.1965,lng:146.5401},
+  {name:"Orange",country:"Australia",lat:-33.2833,lng:149.1000},
+  {name:"Geraldton",country:"Australia",lat:-28.7744,lng:114.6144},
+  {name:"Bowral",country:"Australia",lat:-34.4792,lng:150.4181},
+  {name:"Dubbo",country:"Australia",lat:-32.2479,lng:148.6012},
+  {name:"Busselton",country:"Australia",lat:-33.6532,lng:115.3455},
+  {name:"Bathurst",country:"Australia",lat:-33.4200,lng:149.5775},
+  {name:"Nowra",country:"Australia",lat:-34.8806,lng:150.6053},
+  {name:"Warrnambool",country:"Australia",lat:-38.3840,lng:142.4839},
+  {name:"Kalgoorlie",country:"Australia",lat:-30.7489,lng:121.4658},
+  {name:"Albany",country:"Australia",lat:-35.0275,lng:117.8840},
+  {name:"Mount Gambier",country:"Australia",lat:-37.8284,lng:140.7804},
+  {name:"Alice Springs",country:"Australia",lat:-23.6980,lng:133.8807},
+  {name:"Broken Hill",country:"Australia",lat:-31.9539,lng:141.4539},
+
+  // New Zealand
+  {name:"Auckland",country:"New Zealand",lat:-36.8485,lng:174.7633},
+  {name:"Wellington",country:"New Zealand",lat:-41.2865,lng:174.7762},
+  {name:"Christchurch",country:"New Zealand",lat:-43.5321,lng:172.6362},
+  {name:"Hamilton",country:"New Zealand",lat:-37.7870,lng:175.2793},
+  {name:"Tauranga",country:"New Zealand",lat:-37.6878,lng:176.1651},
+  {name:"Napier",country:"New Zealand",lat:-39.4928,lng:176.9120},
+  {name:"Hastings",country:"New Zealand",lat:-39.6385,lng:176.8465},
+  {name:"Dunedin",country:"New Zealand",lat:-45.8788,lng:170.5028},
+  {name:"Palmerston North",country:"New Zealand",lat:-40.3523,lng:175.6082},
+  {name:"Nelson",country:"New Zealand",lat:-41.2706,lng:173.2840},
+  {name:"Rotorua",country:"New Zealand",lat:-38.1368,lng:176.2497},
+  {name:"New Plymouth",country:"New Zealand",lat:-39.0556,lng:174.0752},
+  {name:"Whangarei",country:"New Zealand",lat:-35.7317,lng:174.3239},
+  {name:"Invercargill",country:"New Zealand",lat:-46.4132,lng:168.3538},
+  {name:"Whanganui",country:"New Zealand",lat:-39.9300,lng:175.0500},
+  {name:"Gisborne",country:"New Zealand",lat:-38.6623,lng:178.0176},
+  {name:"Blenheim",country:"New Zealand",lat:-41.5134,lng:173.9612},
+  {name:"Timaru",country:"New Zealand",lat:-44.3970,lng:171.2550},
+  {name:"Taupo",country:"New Zealand",lat:-38.6857,lng:176.0702},
+  {name:"Masterton",country:"New Zealand",lat:-40.9511,lng:175.6574},
+  {name:"Levin",country:"New Zealand",lat:-40.6225,lng:175.2865},
+  {name:"Ashburton",country:"New Zealand",lat:-43.9006,lng:171.7375},
+  {name:"Queenstown",country:"New Zealand",lat:-45.0312,lng:168.6626},
+
+  // Papua New Guinea
+  {name:"Port Moresby",country:"Papua New Guinea",lat:-9.4438,lng:147.1803},
+  {name:"Lae",country:"Papua New Guinea",lat:-6.7260,lng:146.9846},
+  {name:"Mount Hagen",country:"Papua New Guinea",lat:-5.8569,lng:144.2272},
+  {name:"Madang",country:"Papua New Guinea",lat:-5.2185,lng:145.7952},
+  {name:"Goroka",country:"Papua New Guinea",lat:-6.0827,lng:145.3907},
+  {name:"Wewak",country:"Papua New Guinea",lat:-3.5517,lng:143.6268},
+
+  // Fiji
+  {name:"Suva",country:"Fiji",lat:-18.1416,lng:178.4419},
+  {name:"Lautoka",country:"Fiji",lat:-17.6167,lng:177.4667},
+  {name:"Nadi",country:"Fiji",lat:-17.8000,lng:177.4167},
+  {name:"Labasa",country:"Fiji",lat:-16.4333,lng:179.3667},
+
+  // Solomon Islands
+  {name:"Honiara",country:"Solomon Islands",lat:-9.4280,lng:159.9497},
+  {name:"Auki",country:"Solomon Islands",lat:-8.7667,lng:160.7000},
+
+  // Vanuatu
+  {name:"Port Vila",country:"Vanuatu",lat:-17.7333,lng:168.3167},
+  {name:"Luganville",country:"Vanuatu",lat:-15.5167,lng:167.1667},
+
+  // Samoa
+  {name:"Apia",country:"Samoa",lat:-13.8333,lng:-171.7667},
+
+  // Tonga
+  {name:"Nukuʻalofa",country:"Tonga",lat:-21.1333,lng:-175.2000},
+
+  // Kiribati
+  {name:"Tarawa",country:"Kiribati",lat:1.4333,lng:173.0000},
+
+  // Tuvalu
+  {name:"Funafuti",country:"Tuvalu",lat:-8.5167,lng:179.2167},
+
+  // Nauru
+  {name:"Yaren",country:"Nauru",lat:-0.5477,lng:166.9209},
+
+  // Palau
+  {name:"Koror",country:"Palau",lat:7.3400,lng:134.4800},
+
+  // Marshall Islands
+  {name:"Majuro",country:"Marshall Islands",lat:7.1167,lng:171.2000},
+
+  // Micronesia
+  {name:"Palikir",country:"Micronesia",lat:6.9248,lng:158.1611},
+
+  // New Caledonia (France)
+  {name:"Nouméa",country:"New Caledonia",lat:-22.2758,lng:166.4580},
+
+  // French Polynesia (France)
+  {name:"Papeete",country:"French Polynesia",lat:-17.5350,lng:-149.5696},
+
+  // Guam (US Territory)
+  {name:"Hagåtña",country:"Guam",lat:13.4763,lng:144.7502},
+
+  // Northern Mariana Islands (US Territory)
+  {name:"Saipan",country:"Northern Mariana Islands",lat:15.2123,lng:145.7545},
+
+  // American Samoa (US Territory)
+  {name:"Pago Pago",country:"American Samoa",lat:-14.2794,lng:-170.7006},
+
+  // Hawaii (US - Geographically Oceania)
+  {name:"Honolulu",country:"United States",lat:21.3069,lng:-157.8583},
+  {name:"Hilo",country:"United States",lat:19.7241,lng:-155.0868},
+  {name:"Kailua",country:"United States",lat:21.4022,lng:-157.7394}
 ];
